a281076419152da48c3beb318d76aa3904de0378
axel <axelmartintammekand@gmail.com>
2022-09-15 17:06:59 +0300

    trying to fix data api, will continue later

--- api.js
+++ api.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 8800
+const currencies = [{name:'EURUSD', bid:1.001, ask:1.000}]
 
+app.use(cors())
+
+app.get('/currencies', (req, res) => {
+    res.send(currencies)
+})
 
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import useFetch from "react-fetch-hook";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [rates, setRates] = useState([]);
+
+    useEffect(() => {
+          fetch('http://localhost:8800/currencies')
+              .then(response => {return response.json()})
+              .then(response => {
+                  setRates(response)
+                  ;
+              })
+    })
+
+
+
+    return (
+        <div className="App">
+            <h1>lol</h1>
+        </div>
+    );
 }
 
 export default App;
