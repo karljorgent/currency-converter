d64b7e39f1152933a1458e7ccb59667f4bf1bdb9
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-11-01 23:31:19 +0200

    Added https keys

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const { Server } = require('socket.io');
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: 'https://localhost:3000',
     },
 });
 
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`)
+io.on('connection', (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-
-    socket.on("refresh_rates", () => {
-        socket.broadcast.emit("recive_rates")
-    })
-})
+    socket.on('refresh_rates', () => {
+        socket.broadcast.emit('recive_rates');
+    });
+});
 
 server.listen(3001, () => {
-    console.log("SERVER IS RUNNING");
+    console.log('SERVER IS RUNNING');
 });
